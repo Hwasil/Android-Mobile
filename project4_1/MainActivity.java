93c86e6aace22526d90e10fcaee90dfb295a7be5
Hwasil <127382144+Hwasil@users.noreply.github.com>
2023-03-26 01:23:18 +0900

    Update MainActivity.java

--- project4_1/MainActivity.java
+++ project4_1/MainActivity.java
@@ -88,14 +88,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnDiv.setOnTouchListener(new View.OnTouchListener() {
+        btnDiv.setOnTouchListener(new View.OnTouchListener() { // 나누기
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 num1 = edit1.getText().toString();
                 num2 = edit2.getText().toString();
-
+                
+                // 둘다 공백일 때, 둘다 0일 때 제외. 하나는 공백, 다른 하나는 0일 때 메시지가 어떻게 출력될 것인가 의문
                 if ((num1.equals("")) || (num2.equals(""))) {
+                    /*
+                    if ((num1.equals(0)) || (num2.equals(0))) { 
+                        Toast.makeText(getApplicationContext(), "계산할 수 없습니다.", Toast.LENGTH_SHORT).show();
+                    } */
                     Toast.makeText(getApplicationContext(), "값을 입력하세요!", Toast.LENGTH_SHORT).show();
+                } else if ((num1.equals(0)) || (num2.equals(0))) { //
+                    Toast.makeText(getApplicationContext(), "0은 계산할 수 없습니다.", Toast.LENGTH_SHORT).show();
                 }
                 else {
                     result = Double.parseDouble(num1) / Double.parseDouble(num2);
@@ -105,17 +112,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        btnRem.setOnTouchListener(new View.OnTouchListener() {
+        btnRem.setOnTouchListener(new View.OnTouchListener() { // 나머지
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 num1 = edit1.getText().toString();
                 num2 = edit2.getText().toString();
-
+                
                 if ((num1.equals("")) || (num2.equals(""))) {
                     Toast.makeText(getApplicationContext(), "값을 입력하세요!", Toast.LENGTH_SHORT).show();
                 }
-                else if ((num1.equals(0)) || (num2.equals(0))) {
-                    Toast.makeText(getApplicationContext(), "계산할 수 없습니다.", Toast.LENGTH_SHORT).show();
+                else if ((num1.equals(0)) || (num2.equals(0))) { //
+                    Toast.makeText(getApplicationContext(), "0은 계산할 수 없습니다.", Toast.LENGTH_SHORT).show();
                 }
                 else {
                     result = Double.parseDouble(num1) % Double.parseDouble(num2);
